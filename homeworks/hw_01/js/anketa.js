0832cbed9425c8999c91ac789d198d01535a4f66
j1mb1m <dasha.kozhalo@gmail.com>
2023-03-12 14:55:33 +0300

    task 1

---
+++ homeworks/hw_01/js/anketa.js
@@ -0,0 +1,85 @@
+const ALPHABET_ENG = /^[a-zA-Z]+$/i;
+const ALPHABET_RU = /^[а-яёА-ЯЁ]+$/i;
+
+const msgExp = "Не верно введены данные. \nПовторите ввод.\n";
+const Languages = { NONE: "none", RU: "ru", ENG: "eng" };
+
+let language = Languages.NONE;
+
+let fio = enterFIO();
+let age = enterAge("Сколько вам лет ?"); 
+let gender = confirm('Bаш пол - мужской (Выберите \"ОК\", пол мужской; выберите \"ОТМЕНА\", если пол женский)?');
+
+let information = `ваше ФИО: ${fio}
+ваш возраст в годах: ${age}
+ваш возраст в днях: ${age * 365} 
+через 5 лет вам будет: ${age + 5}
+ваш пол: ${(gender ? "мужской" : "женский")} 
+вы на пенсии: ${((gender ? age >= 63 : age >= 58) == true ? "да" : "нет")}`;
+
+alert(information);
+
+function enterFIO() {
+
+    let lastName = enterStringFromPrompt("Ваша фамилия ?");
+    let firstName = enterStringFromPrompt("Ваше имя ?");
+    let patronymic = enterStringFromPrompt("Ваше отчество ?");
+
+    return `${toCamelCase(lastName)} ${toCamelCase(firstName)} ${toCamelCase(patronymic)} `;
+}
+
+function toCamelCase(str) {
+    let result = String(str).toLowerCase();
+    result = result.charAt(0).toUpperCase() + result.slice(1);
+   
+    return result;
+}
+
+function enterStringFromPrompt(msg) {
+    let str = "";
+    let hasExp = false;
+
+    do {
+        str = prompt((hasExp ? msgExp : "" ) 
+        + `${msg} ${language == Languages.NONE ? "" : "(используйте раскладку \"" + language + "\")"}`);
+
+        hasExp = true;
+
+    } while (!validateString(str));
+
+    return str.trim();
+}
+
+function validateString(value) {
+    console.log(ALPHABET_ENG.test(value));
+    console.log(ALPHABET_RU.test(value));
+    switch (language) {
+        case Languages.ENG: return ALPHABET_ENG.test(value);
+        case Languages.RU: return ALPHABET_RU.test(value);
+        default:
+            if (ALPHABET_ENG.test(value)) {
+                language = Languages.ENG;
+                return true;
+            }
+            else if (ALPHABET_RU.test(value)) {
+                language = Languages.RU;
+                return true;
+            }
+            else return false;
+    }
+    return false;
+}
+
+function enterAge(msg){
+    let str = 18;
+    let hasExp = false;
+
+    do {
+        str = prompt((hasExp ? msgExp : "" ) + `${msg}`, 18);
+        str = Number(str);
+        hasExp = true;
+
+    } while (isNaN(str) || !(str > 0 && str < 130));
+
+    return str;
+}
