f89a57598209c7b052f3659898b6606c56dcd169
Dasha Astakhava <dasha.kozhalo@gmail.com>
2023-04-13 22:36:52 +0300

    rewrited by class task b3

--- homeworks/extra/extra_b4/bfs.js
+++ homeworks/extra/extra_b4/bfs.js
@@ -1,84 +1,90 @@
 "use strict";
 
-const graph = ["ЛУЖА", "МУЗА", "ЛИРА", "МЕХА", "ЛИГА", "ТАРА", "ЛИПА", "ТУРА", "ПАРК", "ЛОЖЬ", "ЛУПА", "ПЛОТ", "МУРА", "ПАУК", "ПАУТ", "ПЛУТ", "ЛОЖА", "СЛОТ", "ПАРА"];
-let search_queue = []; //очередь, куда будем помещать узлы для анализа
-let visited_nodes = []; //массив посещенных узлов
-let distances = {}; //для хранения расстояний, чтобы после поиска взять короткий путь
 
-/* обход графа в ширину */
-function dfs(start, search_value) {
-    start = start.toUpperCase();
-    search_value = search_value.toUpperCase();
-    
-    // очищаем значения
-    search_queue = [];
-    visited_nodes = [];
-    distances = {};
+class Dfs {
+    #graph = ["ЛУЖА", "МУЗА", "ЛИРА", "МЕХА", "ЛИГА", "ТАРА", "ЛИПА", "ТУРА", "ПАРК", "ЛОЖЬ", "ЛУПА", "ПЛОТ", "МУРА", "ПАУК", "ПАУТ", "ПЛУТ", "ЛОЖА", "СЛОТ", "ПАРА"];
+    #search_queue = []; //очередь, куда будем помещать узлы для анализа
+    #visited_nodes = []; //массив посещенных узлов
+    #distances = {}; //для хранения расстояний, чтобы после поиска взять короткий путь
+    #first_word = "";
+    #last_word = "";
 
-    search_queue.push(start); //заполняем очередь первым значением
-    distances[start] = { distance: 0, parents: null }; //указываем его родителя и расстояние к нему
-
-    while (search_queue.length) { //обходим очередь, пока она не закончится
-        let node = search_queue.shift(); // берем первое помещенное значение в очередь для анализа
-        if (!visited_nodes.includes(node)) { //если мы не проходили узел, то проанализируем
-            if (node == search_value) {
-                break;
-            }
-            else {
-                popNeighbors(node, search_value); //добавляем в очередь соседей, т.к. не нашли нужный узел
-            }
-            visited_nodes.push(node); //отмечаем, что узел посетили
-        }
+    constructor(first_word, last_word) {
+        this.#first_word = first_word.toUpperCase();
+        this.#last_word = last_word.toUpperCase();
+        this.#run();
     }
 
-    /* печатаем цепочку прохода к узлу.  */
-    if (search_value in distances) {
-        let result = search_value;
-        let node = search_value;
-        while (distances[node].parents) {
-            node = distances[node].parents;
-            result = node + "-" + result;
+    /* обход графа в ширину */
+    #run() {
+        this.#search_queue.push(this.#first_word); //заполняем очередь первым значением
+        this.#distances[this.#first_word] = { distance: 0, parents: null }; //указываем его родителя и расстояние к нему
+
+        while (this.#search_queue.length) { //обходим очередь, пока она не закончится
+            let node = this.#search_queue.shift(); // берем первое помещенное значение в очередь для анализа
+            if (!this.#visited_nodes.includes(node)) { //если мы не проходили узел, то проанализируем
+                if (node === this.#last_word) {
+                    break;
+                }
+                else {
+                    this.#popNeighbors(node, this.#last_word); //добавляем в очередь соседей, т.к. не нашли нужный узел
+                }
+                this.#visited_nodes.push(node); //отмечаем, что узел посетили
+            }
         }
 
-        return result;
     }
-    else return "Not found!";
 
-}
+    #popNeighbors(node, search_value) {
+        /* если есть совпадение с искомым значением, то берем его */
+        if (this.isSimilarWord(node, search_value)) {
+            this.#search_queue.push(search_value);
+            this.#distances[search_value] = {
+                distance: this.#distances[node].distance += 1,
+                parents: node
+            };
+        }
+        /* нет совпадения с исходным значением, поэтому смотрим соседние узлы */
+        else {
+            this.#graph.forEach(x => {
+                if (!this.#visited_nodes.includes(x) && !this.#search_queue.includes(x) && this.isSimilarWord(x, node)) {
+                    /* если нашли похожие узлы, то ставим в очередь, считаем расстояние и отмечаем родителя */
+                    this.#search_queue.push(x);
+                    this.#distances[x] = {
+                        distance: this.#distances[node].distance += 1,
+                        parents: node
+                    };
+                }
+            });
+        }
 
-function popNeighbors(node, search_value) {
-    /* если есть совпадение с искомым значением, то берем его */
-    if (similarWord(node, search_value)) {
-        search_queue.push(search_value);
-        distances[search_value] = {
-            distance: distances[node].distance += 1,
-            parents: node
-        };
     }
-    /* нет совпадения с исходным значением, поэтому смотрим соседние узлы */
-    else {
-        graph.forEach(x => {
-            if (!visited_nodes.includes(x) && !search_queue.includes(x) && similarWord(x, node)) {
-                /* если нашли похожие узлы, то ставим в очередь, считаем расстояние и отмечаем родителя */
-                search_queue.push(x);
-                distances[x] = {
-                    distance: distances[node].distance += 1,
-                    parents: node
-                };
+
+    isSimilarWord(word1, word2) {
+
+        if (word1.length != word2.length) return false;
+
+        let summ = Array.from(word1).reduce((acc, ch, index) => acc += (ch != word2[index]), 0);
+        if (summ === 1) return true; //должна отличаться только одна буква
+
+        return false;
+    }
+
+    find() {
+        /* печатаем цепочку прохода к узлу.  */
+        if (this.#last_word in this.#distances) {
+            let result = this.#last_word;
+            let node = this.#last_word;
+            while (this.#distances[node].parents) {
+                node = this.#distances[node].parents;
+                result = node + "-" + result;
             }
-        });
+
+            return result;
+        }
+        else return "Not found!";
     }
 
-}
-
-function similarWord(word1, word2) {
-
-    if (word1.length != word2.length) return false;
-
-    let summ = Array.from(word1).reduce((acc, ch, index) => acc += (ch != word2[index]), 0);
-    if (summ === 1) return true; //должна отличаться только одна буква
-
-    return false;
 }
 
 function doTest() {
@@ -92,7 +98,7 @@ function doTest() {
 
 
     for (let iterator of setTests) {
-        let answer = dfs(iterator.first, iterator.last);
+        let answer = new Dfs(iterator.first, iterator.last).find();
         console.log(`${iterator.first} -> ${iterator.last} : ${answer} ${answer === iterator.answer}`);
     }
 }
@@ -102,7 +108,7 @@ function enterTheFormula() {
     let strFirst = prompt("Enter fist word:", "муха");
     let strLast = prompt("Enter last word:", "слон");
 
-    alert(dfs(strFirst, strLast));
+    alert(new Dfs(strFirst, strLast).find());
 }
 
 // Add event listener
